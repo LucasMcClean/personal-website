db6b1e9d2391969c6a367aada32f611a5adfef76
LucasMcClean <121735106+LucasMcClean@users.noreply.github.com>
2024-04-02 17:41:37 +0200

    clean slate

--- src/lib/config.ts
+++
@@ -1,4 +0,0 @@
-import { dev } from "$app/environment";
-
-export const title = "Lucas McClean";
-export const url = dev ? "http://localhost:5173" : "https://lucasmcclean.com/";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,1 @@
-<script>
-  import * as config from "$lib/config";
-</script>
-
-<div>
-  <h1>Lucas McClean</h1>
-  <h2>Level Two Heading</h2>
-  <h3>Level Three Heading</h3>
-  <h4>Level Four Heading</h4>
-  <p>Reach the blog landing page at <a href="/blog">{config.url}/blog</a></p>
-</div>
-
-<style>
-</style>
+<h1>Landing Page</h1>
